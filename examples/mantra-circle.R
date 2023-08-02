265adbb0de4d5c150eafdbb8944d0f82014f41fd
Dmitry O. Gorodnichy <dmitry@gorodnichy.ca>
2023-08-02 09:43:08 -0400

    Update mantra-circle.R

--- examples/mantra-circle.R
+++ examples/mantra-circle.R
@@ -1,23 +1,15 @@
-
-# shift labels ----
+# https://github.com/gorodnichy/r101/blob/main/examples/mantra-circle.R
 
 # This code draws a circle, then draws marks (dot and label beside the dot)
 # on this circle, then connects using line previous dot in a list  to the next one in the list
 # 0 degrees starts from point (0,1) going clockwise.
 # These dots(marks) correspond to the words of "Om-ah-hum-vajra-guru-padma-siddhi-hum" mantra.
 
-# use of data.table and ggplot is demonstrated.
+library(ggplot2)
+library(data.table)
 
-vajra_mantra_cicle <- function (my_colour="green", equalspacing=F) {
-  # Install and load necessary packages
-  if (!requireNamespace("ggplot2")) {
-    install.packages("ggplot2")
-  }
-  if (!requireNamespace("data.table")) {
-    install.packages("data.table")
-  }
-  library(ggplot2)
-  library(data.table)
+my_colour="green"
+vajra_mantra_cicle <- function (my_colour="green", reflected=F, equalspacing=F) {
 
   # Create a data.table to store the information for each mark
   marks_data <- data.table(
@@ -27,11 +19,21 @@ vajra_mantra_cicle <- function (my_colour="green", equalspacing=F) {
     degrees = c(270, 90, 0, 135, 315, 45, 225, 0, 270)
   )
 
+  if (reflected)
+    marks_data <- data.table(
+      id = 1:9,
+      label_eng = c("Om", "Ah", "Hum", "Vajra", "Guru", "Padma", "Siddhi", "Hum", ""),
+      label_san = c("ॐ", "अः", "हूं", "वज्र", "गुरु", "पद्म",       "सिद","हूं",""),
+      degrees = c(270, 90, 0, 135, 225, 45, 315, 180, 270)
+    )
+
+  marks_data [, label :=   paste0(label_san, " \n", label_eng) ]
+
   d = 360 / 5
   if (equalspacing)
     marks_data$degrees = c(240, 120, 0, 2*d, 360-d, d, 3*d, 0, 240)
 
-  marks_data [, label :=   paste0(label_san, " \n", label_eng) ]
+  offset=1.2
 
   # Function to calculate the coordinates on the circle based on degrees
   get_circle_coordinates <- function(degrees, radius = 1) {
@@ -49,45 +51,45 @@ vajra_mantra_cicle <- function (my_colour="green", equalspacing=F) {
 
   # Create a ggplot with a circle and marks
   p <- ggplot() +
-    # Draw the circle
-    geom_path(data = circle_points, aes(x, y), color = "black") +
     coord_fixed() +
-    # theme(plot.margin = unit(rep(0, 4), "cm"))  + # Remove plot margins
-    theme_void()
-
+    theme(plot.margin = unit(rep(0, 4), "cm"))  + # Remove plot margins
+    theme_void() +
+    # Draw the circle
+    geom_path(data = circle_points, aes(x, y), color = "black")
 
   # Add the marks as points and labels (positioned outside the circle)
   p <- p +
-    geom_point(data = marks_data[1:7], aes(x, y), size = 3, color = my_colour) +
-    geom_text(data = marks_data[1:7], aes(x = 1.2 * x, y = 1.2 * y, label = label), color = my_colour)
+    geom_point(data = marks_data[1:9], aes(x, y), size = 3, color = my_colour) +
+    geom_text(data = marks_data[1:9], aes(x = offset * x, y = offset * y, label = label), color = my_colour)
 
   # Add lines connecting the dots (excluding the last two)
   p <- p +
     geom_segment(
-      data = marks_data[1:7],  # Exclude last two rows
+      data = marks_data[1:9],  # Exclude last two rows
       aes(x = x, y = y, xend = shift(x, type = "lead"), yend = shift(y, type = "lead")),
       color = my_colour
     )
 
-  # Connect dot 7 to dot 8 (which is 3)
-  p <- p +
-    geom_segment(
-      data = marks_data[c(7, 8)],
-      aes(x = x, y = y, xend = x[1], yend = y[1]),
-      color = my_colour
-    )
-
-  # Connect dot 8 (which is 3) to dot 9 (which is 1)
-  p <- p +
-    geom_segment(
-      data = marks_data[c(8, 9)],
-      aes(x = x, y = y, xend = x[1], yend = y[1]),
-      color = my_colour
-    )
+  # # Connect dot 7 to dot 8 (which is 3)
+  # p <- p +
+  #   geom_segment(
+  #     data = marks_data[c(7, 8)],
+  #     aes(x = x, y = y, xend = x[1], yend = y[1]),
+  #     color = my_colour
+  #   )
+  #
+  # # Connect dot 8 (which is 3) to dot 9 (which is 1)
+  # p <- p +
+  #   geom_segment(
+  #     data = marks_data[c(8, 9)],
+  #     aes(x = x, y = y, xend = x[1], yend = y[1]),
+  #     color = my_colour
+  #   )
 
   print(p)
 
 
 }
 vajra_mantra_cicle()
-vajra_mantra_cicle("red", T)
+vajra_mantra_cicle("red", reflected=T)
+vajra_mantra_cicle("red", equalspacing = T)
